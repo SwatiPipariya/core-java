e5cb12cff6810919cdc0510e5dfd043c847771bf
Swati <swati.pipariya6171@gmail.com>
2024-02-09 15:32:12 +0530

    Studied Primitive data types

---
+++ src/core_Java/PrimitiveDataTypes.java
@@ -0,0 +1,37 @@
+package core_Java;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		
+		char a = 'H';
+		
+		byte b = 2;
+		
+		short c = 43;
+		
+		int d = 56;
+		
+		float e = 1.43f;
+		
+		double f = 9.234343432d;
+		
+		boolean g = true;
+		
+		System.out.println("char :" + a);
+		System.out.println("byte :" + b);
+		System.out.println("short :" + c);
+		System.out.println("int :" + d);
+		System.out.println("float :" + e);
+		System.out.println("double :" + f);
+		System.out.println("boolean :" + g); 
+		
+		
+		
+		
+		
+		
+
+	}
+
+}

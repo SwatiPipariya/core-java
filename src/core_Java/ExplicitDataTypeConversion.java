961ef2c612e11643fff185296add448669a4244d
swati <swati.pipariya6171@gmail.com>
2024-02-11 19:18:43 +0530

    Implicit and explicit data type conversion

---
+++ src/core_Java/ExplicitDataTypeConversion.java
@@ -0,0 +1,23 @@
+package core_Java;
+
+public class ExplicitDataTypeConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double a = 109.32;
+		System.out.println("Double Representation "+a);
+		
+		float f = (float)a;
+		System.out.println("Float Representation "+f);
+		
+		long b = (long)f;
+		System.out.println("Long Representation "+b);
+		
+		int c = (int)b;
+		System.out.println("Int Representation "+c);
+		
+
+	}
+
+}
---
+++ src/core_Java/ImplicitDataTypeConversion.java
@@ -0,0 +1,16 @@
+package core_Java;
+
+public class ImplicitDataTypeConversion {
+    
+	public static void main(String[] args) {
+		int a = 40;
+		System.out.println("Int Representation:" +a);
+		
+		long b = a;
+		System.out.println("Long Representation:" +b);
+		
+		float c = b;
+		System.out.println("Float Representation:" +c);
+		
+	}
+}

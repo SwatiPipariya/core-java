7cd62af30a1aa4c855159353934af986e28b3b19
swati <swati.pipariya6171@gmail.com>
2024-02-12 22:57:42 +0530

    Array demo

---
+++ src/core_Java/HelloWorld.java
@@ -0,0 +1,12 @@
+package core_Java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// The reason behind writing static keyword in java is that whenever we want to run a class
+		// without creating object of that class at that time we use static keyword
+		System.out.println("Hello world");
+
+	}
+
+}

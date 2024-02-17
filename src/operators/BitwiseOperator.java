90507547dbc03db74412280ae5f6131b43bf3ab1
swati <swati.pipariya6171@gmail.com>
2024-02-17 23:35:15 +0530

    Studied  Operators

---
+++ src/operators/BitwiseOperator.java
@@ -0,0 +1,22 @@
+package operators;
+
+public class BitwiseOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		//Initial Values
+		int a = 5;
+		int b = 7;
+		
+		// Bitwise AND
+		// 0101 & 0111 = 0101 = 5
+		System.out.println("a&b = " + (a & b));
+		
+		// Bitwise OR
+		// 0101 | 0111 = 0111 = 7   
+		System.out.println("a|b = " + (a | b));
+		
+	}
+
+}
---
+++ src/operators/LogicalOperator.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int value1 = 1;
+        int value2 = 2;
+        
+        if((value1 == 1) && (value2 == 2)) {
+        	System.out.println("Value1 is 1 AND value2 is 2");
+        }
+        
+        if((value1 == 1) || (value2 == 1)) {
+        	System.out.println("Value1 is 1 OR value2 is 1");
+        }
+	}
+
+}

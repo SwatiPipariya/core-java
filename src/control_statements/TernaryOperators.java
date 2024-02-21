f310c371b32f60a2f9153baf6cc7d5ae3860f0f7
swati <swati.pipariya6171@gmail.com>
2024-02-21 15:40:05 +0530

    Ternary operators

---
+++ src/control_statements/TernaryOperators.java
@@ -0,0 +1,23 @@
+package control_statements;
+
+public class TernaryOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 3;
+		int b = 2;
+		int result;
+		
+		result = a < b ? a : b;
+		
+//		if(a < b ) {
+//			result = a;
+//		} else {
+//			result = b;
+//		}
+        
+		System.out.println(result);
+	}
+
+}

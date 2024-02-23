408a2d7c46e8b2440e8dbff4ac36980b1367f414
swati <swati.pipariya6171@gmail.com>
2024-02-23 23:10:45 +0530

    Loops

---
+++ src/loops/DoWhileDemo.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class DoWhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           int count = 1;
+           
+           do {
+        	   System.out.println("Count is: " + count);
+        	   count++;
+           } while (count < 10);
+	}
+
+}

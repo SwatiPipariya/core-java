3e6e3198526b78bc58b9413901659340872b9bdf
swati <swati.pipariya6171@gmail.com>
2024-02-24 15:05:04 +0530

    For Loop

---
+++ src/loops/WhileDemo.java
@@ -0,0 +1,16 @@
+package loops;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		int count = 1;
+		
+		while (count < 10) {
+			System.out.println("Count is: " + count);
+			count++;
+		}
+	}
+
+}

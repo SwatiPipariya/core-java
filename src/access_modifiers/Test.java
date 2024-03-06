df090f57a21f0e8a691d2d7afbc3c9cbbe76278c
swati <swati.pipariya6171@gmail.com>
2024-03-06 15:55:52 +0530

    Access Modifiers

---
+++ src/access_modifiers/Test.java
@@ -0,0 +1,9 @@
+package access_modifiers;
+
+public class Test {
+
+	public void display() {
+		System.out.println("Hello World!!");
+	}
+
+}
---
+++ src/access_modifiers/TestDemo.java
@@ -0,0 +1,11 @@
+package access_modifiers;
+
+public class TestDemo {
+	
+	public static void main(String[] args) {
+       Test t = new Test();
+       t.display();
+		
+	}
+
+}

f90453b2c548a6936a47d8fcd61b42a94bfda39a
swati <swati.pipariya6171@gmail.com>
2024-03-23 13:58:00 +0530

    Studied access modifiers

---
+++ src/test_access_modifiers/Basic.java
@@ -0,0 +1,12 @@
+package test_access_modifiers;
+
+import access_modifiers.Test;
+
+public class Basic {
+
+	public static void main(String[] args) {
+      Test t = new Test();
+      t.display();
+	}
+
+}
